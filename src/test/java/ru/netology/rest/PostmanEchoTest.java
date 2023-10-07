87fe3b815cbe683abe760c050df34b153a7513de
Bolotjanovna <toksonbaeva2104@gmail.com>
2023-10-07 23:09:39 +0500

    Postman echo

--- src/test/java/ru/netology/rest/PostmanEchoTest.java
+++ src/test/java/ru/netology/rest/PostmanEchoTest.java
@@ -15,7 +15,7 @@ public class PostmanEchoTest {
                 .post("/post")
                 .then()
                 .statusCode(200)
-                .body("data", equalTo("Hi")) ;
+                .body("data", equalTo("H1")) ;
 
 
     }
